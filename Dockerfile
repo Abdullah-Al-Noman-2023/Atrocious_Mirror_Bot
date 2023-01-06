e36be9caa9d48d9006e9ce71ac17cc119def4ca7
Abdullah-Al-Noman-2023 <122092932+Abdullah-Al-Noman-2023@users.noreply.github.com>
2023-01-06 17:20:45 +0600

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM anasty17/mltb:heroku
+FROM codewithweeb/weebzone:stable
 
 WORKDIR /usr/src/app
 RUN chmod 777 /usr/src/app
 
 COPY . .
-RUN pip3 install --no-cache-dir -r requirements.txt
+RUN pip3 install --no-cache-dir -r requirements.txt 
 
 CMD ["bash", "start.sh"]
--- update.py
+++ update.py
@@ -13,22 +13,6 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
-
 load_dotenv('config.env', override=True)
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
@@ -37,7 +21,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/arshsisodiya/helios-mirror"
+    UPSTREAM_REPO = "https://github.com/AL-Noman-23/Helios_Mirror_Bot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
@@ -60,4 +44,3 @@ if update.returncode == 0:
     log_info('Successfully updated with latest commit from UPSTREAM_REPO')
 else:
     log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
-
